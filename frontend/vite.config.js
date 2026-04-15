2f2d12c0bf3c529a381261f9949ec32ec0538dd8
bhavik <bhavik0726@gmail.com>
2026-04-15 11:54:07 +0530

    some changes before deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        lobby: resolve(__dirname, 'lobby.html'),
+        selectCharacter: resolve(__dirname, 'select-character.html'),
+      },
+    },
+  },
 })
